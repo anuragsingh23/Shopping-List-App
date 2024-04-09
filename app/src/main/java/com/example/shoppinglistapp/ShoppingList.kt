c16fdf501a1153e216416dc5813c4b2a9ceddc3c
Shekhawat_anurag28 <69235530+anuragsingh23@users.noreply.github.com>
2024-04-09 23:19:54 +0530

    building alertDialog 2 text and outlinetext addde

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -3,10 +3,14 @@ package com.example.shoppinglistapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,13 +33,26 @@ fun ShoppingListApp(){
     var sItems by remember {
         mutableStateOf(listOf<ShoppingItems>())
     }
+    var showAlert by remember {
+        mutableStateOf(false)
+    }
+
+    var itemName by remember {
+        mutableStateOf("")
+    }
+
+    var itemQuantity by remember {
+        mutableStateOf("")
+    }
+
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         // add Item Button
         Button(
-            onClick = { },
+            onClick = { showAlert = true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Add Item")
@@ -50,5 +67,34 @@ fun ShoppingListApp(){
 
             }
         }
+    }
+
+    if (showAlert){
+       AlertDialog(onDismissRequest = { showAlert = false },
+           confirmButton = { /*TODO*/ },
+           title = { Text(text = "Add Shopping Item")},
+           text = {
+               Column {
+                   OutlinedTextField(value = itemName, onValueChange = {
+                       itemName = it
+                   },
+                       singleLine = true,
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(8.dp),
+                       label = { Text(text = "Item Name")}
+                   )
+                   OutlinedTextField(value = itemQuantity, onValueChange = {
+                       itemQuantity = it
+                   },
+                       singleLine = true,
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(8.dp),
+                       label = { Text(text = "Item Quantity")}
+                   )
+               }
+           }
+       )
     }
 }

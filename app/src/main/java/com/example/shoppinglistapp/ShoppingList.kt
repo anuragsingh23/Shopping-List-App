8efebd5209a454dad4852eae00b5c31b4ded2b13
Shekhawat_anurag28 <69235530+anuragsingh23@users.noreply.github.com>
2024-04-11 18:28:18 +0530

    lazy column added to ui

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,11 +24,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class ShoppingListData class ShoppingItems(
     val id: Int,
-    var item: String,
+    var name: String,
     var isEditing: Boolean = false,
     var quantity: Int
 )
@@ -66,7 +70,7 @@ fun ShoppingListApp(){
                 .padding(16.dp)
         ){
             items(sItems){
-
+                ShoppingListItem(items = it , onEditClick = {  }, onDeleteClick = {})
             }
         }
     }
@@ -84,7 +88,7 @@ fun ShoppingListApp(){
                                    if (itemName.isNotBlank()){
                                        val newItem = ShoppingItems(
                                            id = sItems.size + 1 ,
-                                           item = itemName,
+                                           name = itemName,
                                            quantity = itemQuantity.toInt()
                                        )
                                        sItems = sItems + newItem
@@ -125,3 +129,24 @@ fun ShoppingListApp(){
        )
     }
 }
+
+@Composable
+fun ShoppingListItem(
+    items: ShoppingItems,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+  Row (
+      modifier = Modifier
+          .padding(8.dp)
+          .fillMaxWidth()
+          .border(
+              border = BorderStroke(2.dp, Color.Cyan),
+              shape = RoundedCornerShape(20)
+          )
+  ){
+      Text(text = items.name)
+
+  }
+}
+
